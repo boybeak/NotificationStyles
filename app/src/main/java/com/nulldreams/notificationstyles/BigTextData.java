1539e0c510b700faadb089a78a1e7700244e8ab0
Beak Gao <Beak Gao>
2017-01-18 17:46:33 +0800

    add some code

---
+++ app/src/main/java/com/nulldreams/notificationstyles/BigTextData.java
@@ -0,0 +1,43 @@
+package com.nulldreams.notificationstyles;
+
+/**
+ * Created by boybe on 2017/1/18.
+ */
+
+public class BigTextData {
+    public static final String BIG_TEXT =
+            "Imagine there's no heaven\n" +
+            "It's easy if you try\n" +
+            "No hell below us\n" +
+            "Above us only sky\n" +
+            "Imagine all the people\n" +
+            "Living for today... Aha-ah...\n" +
+            "\n" +
+            "Imagine there's no countries\n" +
+            "It isn't hard to do\n" +
+            "Nothing to kill or die for\n" +
+            "And no religion, too\n" +
+            "Imagine all the people\n" +
+            "Living life in peace... You...\n" +
+            "\n" +
+            "You may say I'm a dreamer\n" +
+            "But I'm not the only one\n" +
+            "I hope someday you'll join us\n" +
+            "And the world will be as one\n" +
+            "\n" +
+            "Imagine no possessions\n" +
+            "I wonder if you can\n" +
+            "No need for greed or hunger\n" +
+            "A brotherhood of man\n" +
+            "Imagine all the people\n" +
+            "Sharing all the world... You...\n" +
+            "\n" +
+            "You may say I'm a dreamer\n" +
+            "But I'm not the only one\n" +
+            "I hope someday you'll join us\n" +
+            "And the world will live as one";
+    public static final String BIG_TEXT_TITLE =
+            "Imagine - JOHN LENNON";
+    public static final String BIG_TEXT_SUMMARY =
+            "https://www.youtube.com/watch?v=yRhq-yO1KN8";
+}
--- app/src/main/java/com/nulldreams/notificationstyles/MainActivity.java
+++ app/src/main/java/com/nulldreams/notificationstyles/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nulldreams.notificationstyles;
 
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void showBigText (View view) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        android.support.v4.app.NotificationCompat.BigTextStyle bigTextStyle = new android.support.v4.app.NotificationCompat.BigTextStyle(builder);
+        bigTextStyle.bigText(BigTextData.BIG_TEXT);
+        bigTextStyle.setBigContentTitle(BigTextData.BIG_TEXT_TITLE);
+        bigTextStyle.setSummaryText(BigTextData.BIG_TEXT_SUMMARY);
+        builder.setStyle(bigTextStyle);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("BigText ContentTitle");
+        builder.setContentText("BigText ContentText");
+        builder.setTicker("BigText Ticker");
+
+        showNotificationCompat(builder);
+    }
+
+    private void showNotificationCompat (NotificationCompat.Builder builder) {
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(1, builder.build());
     }
 }
